e84208709d4c8ccedf765bbd5ccddd45ed957044
Charles G <charles.grigny@ipsa.fr>
2022-06-09 19:31:45 +0200

    Ajout de la Navigation

--- Essais_React/src/App.js
+++ Essais_React/src/App.js
@@ -1,3 +1,18 @@
-function App() {
-  return <h1>hello</h1>;};
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import About from './pages/About';
+import Home from './pages/Home';
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        {/* path * <=> rien de connu */}
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
 export default App;
---
+++ Essais_React/src/pages/About.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Navigation />
+            <h1>A PROPOS</h1>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus ut, quia ducimus similique
+                veritatis dicta perferendis numquam assumenda reiciendis labore, in cum at esse aspernatur minus porro.
+                Tenetur explicabo impedit cumque, obcaecati quidem voluptas minus minima aspernatur ratione delectus perspiciatis
+                necessitatibus ducimus iure at id animi fuga error doloremque dolorum itaque facilis recusandae suscipit commodi in.
+                Eos libero voluptatibus ad doloribus ex neque, repellendus pariatur similique nesciunt vero. Non, dolore velit nobis
+                distinctio obcaecati eum, eius odit repellat tenetur magni quidem? Inventore, eveniet rem optio ratione, repudiandae
+                praesentium asperiores aliquam sequi provident enim reprehenderit tempora ipsam deleniti! Mollitia, exercitationem repellat.</p>
+            <br />
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis eaque corrupti dolor deleniti labore officia
+                aliquam dignissimos ab reiciendis porro sint iusto voluptas culpa facere nemo mollitia totam recusandae laborum
+                dicta perspiciatis, laudantium minus animi, ad alias! Nihil error, nulla porro distinctio aut esse assumenda iure
+                consequuntur omnis alias officiis?</p>
+        </div>
+    );
+};
+
+export default About;
